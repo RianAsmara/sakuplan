8ed2c0d1de19b9081db7c4f8694ff19fb9337b4e
asmaraputra <rputra711@gmail.com>
2026-08-04 21:26:50 +0700

    feat(mobile): add SafeAreaProvider to root layout

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -2,6 +2,7 @@ import { Slot } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { TamaguiProvider, Theme, YStack, Spinner } from 'tamagui'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 import tamaguiConfig from '../tamagui.config'
 import { useAppFontsLoaded } from '../src/theme/fonts'
 
@@ -12,24 +13,28 @@ export default function RootLayout() {
 
   if (!fontsLoaded) {
     return (
-      <TamaguiProvider config={tamaguiConfig} defaultTheme="light">
-        <Theme name="light">
-          <YStack flex={1} alignItems="center" justifyContent="center" backgroundColor="$background">
-            <Spinner size="large" color="$primary" />
-          </YStack>
-        </Theme>
-      </TamaguiProvider>
+      <SafeAreaProvider>
+        <TamaguiProvider config={tamaguiConfig} defaultTheme="light">
+          <Theme name="light">
+            <YStack flex={1} alignItems="center" justifyContent="center" backgroundColor="$background">
+              <Spinner size="large" color="$primary" />
+            </YStack>
+          </Theme>
+        </TamaguiProvider>
+      </SafeAreaProvider>
     )
   }
 
   return (
-    <TamaguiProvider config={tamaguiConfig} defaultTheme="light">
-      <Theme name="light">
-        <QueryClientProvider client={queryClient}>
-          <StatusBar style="dark" />
-          <Slot />
-        </QueryClientProvider>
-      </Theme>
-    </TamaguiProvider>
+    <SafeAreaProvider>
+      <TamaguiProvider config={tamaguiConfig} defaultTheme="light">
+        <Theme name="light">
+          <QueryClientProvider client={queryClient}>
+            <StatusBar style="dark" />
+            <Slot />
+          </QueryClientProvider>
+        </Theme>
+      </TamaguiProvider>
+    </SafeAreaProvider>
   )
 }
