87f3fe30abb2aaeb8972cacc7a94ca14ed28e4be
asmaraputra <rputra711@gmail.com>
2026-08-17 10:28:07 +0700

    feat(mobile): load Fraunces/IBM Plex Sans/IBM Plex Mono instead of Inter

--- mobile/src/theme/fonts.ts
+++ mobile/src/theme/fonts.ts
@@ -1,17 +1,19 @@
+import { useFonts } from 'expo-font'
+import { Fraunces_600SemiBold } from '@expo-google-fonts/fraunces'
 import {
-  useFonts as useInter,
-  Inter_400Regular,
-  Inter_500Medium,
-  Inter_600SemiBold,
-  Inter_700Bold,
-} from '@expo-google-fonts/inter'
+  IBMPlexSans_400Regular,
+  IBMPlexSans_500Medium,
+  IBMPlexSans_600SemiBold,
+} from '@expo-google-fonts/ibm-plex-sans'
+import { IBMPlexMono_500Medium } from '@expo-google-fonts/ibm-plex-mono'
 
 export function useAppFontsLoaded(): boolean {
-  const [interLoaded] = useInter({
-    Inter_400Regular,
-    Inter_500Medium,
-    Inter_600SemiBold,
-    Inter_700Bold,
+  const [fontsLoaded] = useFonts({
+    Fraunces_600SemiBold,
+    IBMPlexSans_400Regular,
+    IBMPlexSans_500Medium,
+    IBMPlexSans_600SemiBold,
+    IBMPlexMono_500Medium,
   })
-  return interLoaded
+  return fontsLoaded
 }
